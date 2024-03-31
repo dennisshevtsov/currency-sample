714d0704d6b870bfae0a1eeb2f3558fa7933d72b
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-03-31 07:37:20 -0700

    add ratio

---
+++ src/MeneySample/MinorUnitRatio.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace MeneySample;
+
+public readonly struct MinorUnitRatio
+{
+  private MinorUnitRatio(ulong ratio) => Ratio = ratio;
+  private ulong Ratio { get; }
+
+  public override string ToString() => Ratio.ToString();
+
+  public static readonly MinorUnitRatio Ratio100          = new(ratio: 100);
+  public static readonly MinorUnitRatio UnitedStatesCents = MinorUnitRatio.Ratio100;
+  public static readonly MinorUnitRatio EuroCents         = MinorUnitRatio.Ratio100;
+  public static readonly MinorUnitRatio BelarusianKopecks = MinorUnitRatio.Ratio100;
+  public static readonly MinorUnitRatio RussianKopecks    = MinorUnitRatio.Ratio100;
+}
