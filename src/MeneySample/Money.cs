f8c0e809545261860eb7c2d58e0c7989a9e732b1
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-04-02 12:39:13 -0700

    hide props

--- src/MeneySample/Money.cs
+++ src/MeneySample/Money.cs
@@ -4,18 +4,24 @@
 
 namespace MeneySample;
 
-public readonly record struct Money(ulong TotalMinorUnits, Currency Currency)
+public readonly struct Money
 {
   private const ulong MinorUnitRatio = 100UL;
 
-  public ulong MajorUnits => TotalMinorUnits / Money.MinorUnitRatio;
-  public ulong MinorUnits => TotalMinorUnits % Money.MinorUnitRatio;
-  public decimal TotalMajorUnits => (decimal)TotalMinorUnits / MinorUnitRatio;
+  private Money(Currency currency, ulong totalMinorUnits) => (Currency, TotalMinorUnits) = (currency, totalMinorUnits);
+
+  private Currency Currency { get; }
+  private ulong TotalMinorUnits { get; }
+
+  private ulong MajorUnits => TotalMinorUnits / Money.MinorUnitRatio;
+  private ulong MinorUnits => TotalMinorUnits % Money.MinorUnitRatio;
 
   public override string ToString() => $"{MajorUnits}.{MinorUnits} {Currency}";
 
-  public static Money UnitedStatesDollar(ushort cents) => new(TotalMinorUnits: cents, Currency: Currency.UnitedStatesDollar);
-  public static Money Euro(ushort cents) => new(TotalMinorUnits: cents, Currency: Currency.Euro);
-  public static Money BelarusianRouble(ushort kopecks) => new(TotalMinorUnits: kopecks, Currency: Currency.BelarusianRouble);
-  public static Money RussianRouble(ushort kopecks) => new(TotalMinorUnits: kopecks, Currency: Currency.RussianRouble);
+  public static Money None { get; }
+
+  public static Money UnitedStatesDollar(ushort cents) => new(currency: Currency.UnitedStatesDollar, totalMinorUnits: cents);
+  public static Money Euro(ushort cents)               => new(currency: Currency.Euro, totalMinorUnits: cents);
+  public static Money BelarusianRouble(ushort kopecks) => new(currency: Currency.BelarusianRouble, totalMinorUnits: kopecks);
+  public static Money RussianRouble(ushort kopecks)    => new(currency: Currency.RussianRouble, totalMinorUnits: kopecks);
 }
