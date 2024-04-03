3f0306bb6c6fcf1ff392d6cc8232b117f628f289
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-04-03 12:57:03 -0700

    add tests

---
+++ test/MoneySample.Test/CurrencyTest.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using MeneySample;
+
+namespace MoneySample.Test;
+
+[TestClass]
+public sealed class CurrencyTest
+{
+  [TestMethod]
+  public void Equals_UnitedStatesDollarAndUnitedStatesDollar_TrueReturned()
+  {
+    // Arrange
+    Currency dollar1 = Currency.UnitedStatesDollar;
+    Currency dollar2 = Currency.UnitedStatesDollar;
+
+    // Act
+    bool result = dollar1.Equals(dollar2);
+
+    // Assert
+    Assert.IsTrue(result);
+  }
+}
