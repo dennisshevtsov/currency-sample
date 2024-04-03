b51754315c12cdd85a42de110055cbb662614ee1
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-04-03 13:21:38 -0700

    add tests

--- test/MoneySample.Test/CurrencyTest.cs
+++ test/MoneySample.Test/CurrencyTest.cs
@@ -7,6 +7,7 @@ using MeneySample;
 namespace MoneySample.Test;
 
 [TestClass]
+
 public sealed class CurrencyTest
 {
   [TestMethod]
@@ -60,6 +61,118 @@ public sealed class CurrencyTest
 
     // Act
     bool result = dollar.Equals(rouble);
+
+    // Assert
+    Assert.IsFalse(result);
+  }
+
+  [TestMethod]
+  public void Equals_EuroAndUnitedStatesDollar_FalseReturned()
+  {
+    // Arrange
+    Currency euro = Currency.Euro;
+    Currency dollar = Currency.UnitedStatesDollar;
+
+    // Act
+    bool result = euro.Equals(dollar);
+
+    // Assert
+    Assert.IsFalse(result);
+  }
+
+  [TestMethod]
+  public void Equals_EuroAndUnitedStatesEuro_TrueReturned()
+  {
+    // Arrange
+    Currency euro1 = Currency.Euro;
+    Currency euro2 = Currency.Euro;
+
+    // Act
+    bool result = euro1.Equals(euro2);
+
+    // Assert
+    Assert.IsTrue(result);
+  }
+
+  [TestMethod]
+  public void Equals_EuroAndBelarusianRouble_FalseReturned()
+  {
+    // Arrange
+    Currency euro = Currency.Euro;
+    Currency rouble = Currency.BelarusianRouble;
+
+    // Act
+    bool result = euro.Equals(rouble);
+
+    // Assert
+    Assert.IsFalse(result);
+  }
+
+  [TestMethod]
+  public void Equals_EuroAndRussianRouble_FalseReturned()
+  {
+    // Arrange
+    Currency euro = Currency.Euro;
+    Currency rouble = Currency.RussianRouble;
+
+    // Act
+    bool result = euro.Equals(rouble);
+
+    // Assert
+    Assert.IsFalse(result);
+  }
+
+  [TestMethod]
+  public void Equals_BelarusianRoubleAndUnitedStatesDollar_FalseReturned()
+  {
+    // Arrange
+    Currency rouble = Currency.BelarusianRouble;
+    Currency dollar = Currency.UnitedStatesDollar;
+
+    // Act
+    bool result = rouble.Equals(dollar);
+
+    // Assert
+    Assert.IsFalse(result);
+  }
+
+  [TestMethod]
+  public void Equals_BelarusianRoubleAndUnitedStatesEuro_FalseReturned()
+  {
+    // Arrange
+    Currency rouble = Currency.BelarusianRouble;
+    Currency euro   = Currency.Euro;
+
+    // Act
+    bool result = rouble.Equals(euro);
+
+    // Assert
+    Assert.IsFalse(result);
+  }
+
+  [TestMethod]
+  public void Equals_BelarusianRoubleAndBelarusianRouble_TrueReturned()
+  {
+    // Arrange
+    Currency rouble1 = Currency.BelarusianRouble;
+    Currency rouble2 = Currency.BelarusianRouble;
+
+    // Act
+    bool result = rouble1.Equals(rouble2);
+
+    // Assert
+    Assert.IsTrue(result);
+  }
+
+  [TestMethod]
+  public void Equals_BelarusianRoubleAndRussianRouble_FalseReturned()
+  {
+    // Arrange
+    Currency belarusianRouble = Currency.BelarusianRouble;
+    Currency roussianRouble   = Currency.RussianRouble;
+
+    // Act
+    bool result = belarusianRouble.Equals(roussianRouble);
 
     // Assert
     Assert.IsFalse(result);
