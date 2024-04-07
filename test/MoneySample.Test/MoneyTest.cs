bfb3feb1bc9758bb4de3053c142f62c9821c16cf
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-04-07 08:38:03 -0700

    test equals

--- test/MoneySample.Test/MoneyTest.cs
+++ test/MoneySample.Test/MoneyTest.cs
@@ -50,6 +50,20 @@ public sealed class MoneyTest
   }
 
   [TestMethod]
+  public void Equals_UnitedStatesDollarsAndObjectWithPackedEuro_ExceptionThrown()
+  {
+    // Arrange
+    Money  money1 = Money.UnitedStatesDollar(cents: 1000UL);
+    object money2 = Money.Euro(cents: 2000UL);
+
+    // Act
+    Action action = () => money1.Equals(money2);
+
+    // Arrange
+    Assert.ThrowsException<InvalidOperationException>(action);
+  }
+
+  [TestMethod]
   public void Equals_SameMoney_TrueReturned()
   {
     // Arrange
@@ -75,5 +89,19 @@ public sealed class MoneyTest
 
     // Arrange
     Assert.IsFalse(result);
+  }
+
+  [TestMethod]
+  public void Equals_UnitedStatesDollarsAndEuro_ExceptionThrown()
+  {
+    // Arrange
+    Money money1 = Money.UnitedStatesDollar(cents: 1000UL);
+    Money money2 = Money.Euro(cents: 2000UL);
+
+    // Act
+    Action action = () => money1.Equals(money2);
+
+    // Arrange
+    Assert.ThrowsException<InvalidOperationException>(action);
   }
 }
