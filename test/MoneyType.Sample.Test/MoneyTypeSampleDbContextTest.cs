bbd9a47e72ebe31b0e439f08c2ccd8b524e80ecc
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-04-19 06:18:25 -0700

    test where

--- test/MoneyType.Sample.Test/MoneyTypeSampleDbContextTest.cs
+++ test/MoneyType.Sample.Test/MoneyTypeSampleDbContextTest.cs
@@ -32,7 +32,7 @@ public sealed class MoneyTypeSampleDbContextTest
     {
       ProductId = productId,
       Name      = "Test product",
-      Price     = Money.USD(1500UL),
+      Price     = Money.USD(cents: 1500UL),
     };
     _dbContext.Add(productToSave);
 
@@ -48,5 +48,57 @@ public sealed class MoneyTypeSampleDbContextTest
     Assert.IsNotNull(savedProduct);
     Assert.AreEqual(productToSave.Name , savedProduct.Name );
     Assert.AreEqual(productToSave.Price, savedProduct.Price);
+  }
+
+  [TestMethod]
+  public async Task ToListAsync_EqualToUSD100_ProductByUSD100Returned()
+  {
+    // Arrange
+    ProductEntity productByUSD100 = new()
+    {
+      ProductId = Guid.NewGuid(),
+      Name      = "Product by USD100",
+      Price     = Money.USD(cents: 10000UL),
+    };
+    _dbContext.Add(productByUSD100);
+
+    ProductEntity productByUSD200 = new()
+    {
+      ProductId = Guid.NewGuid(),
+      Name      = "Product by USD200",
+      Price     = Money.USD(cents: 20000UL),
+    };
+    _dbContext.Add(productByUSD200);
+
+    ProductEntity productByEUR100 = new()
+    {
+      ProductId = Guid.NewGuid(),
+      Name      = "Product by EUR100",
+      Price     = Money.EUR(cents: 10000UL),
+    };
+    _dbContext.Add(productByEUR100);
+
+    ProductEntity productByEUR200 = new()
+    {
+      ProductId = Guid.NewGuid(),
+      Name      = "Product by EUR200",
+      Price     = Money.EUR(cents: 20000UL),
+    };
+    _dbContext.Add(productByEUR200);
+
+    await _dbContext.SaveChangesAsync();
+
+    // Act
+    Money usd100 = Money.USD(cents: 10000UL);
+    List<ProductEntity> products = await _dbContext.Set<ProductEntity>()
+                                                   .AsNoTracking()
+                                                   .Where(entity => entity.Price == usd100)
+                                                   .ToListAsync();
+
+    // Assert
+    Assert.AreEqual(1, products.Count);
+    Assert.AreEqual(productByUSD100.ProductId, products[0].ProductId);
+    Assert.AreEqual(productByUSD100.Name     , products[0].Name);
+    Assert.AreEqual(productByUSD100.Price    , products[0].Price);
   }
 }
