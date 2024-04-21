4093a7c598a9189fc4ad51e49dba2d8b50229376
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-04-21 03:31:09 -0700

    remove log

--- sample/MoneyType.Sample/MoneyTypeSampleDbContext.cs
+++ sample/MoneyType.Sample/MoneyTypeSampleDbContext.cs
@@ -12,7 +12,6 @@ public sealed class MoneyTypeSampleDbContext : DbContext
   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
   {
     optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=money-type-sample-db;Username=dev;Password=dev");
-    optionsBuilder.LogTo(Console.WriteLine);
   }
 
   protected override void OnModelCreating(ModelBuilder modelBuilder)
